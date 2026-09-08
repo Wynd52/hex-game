0536ceef04fb0732ff713888087672062a425100
Wynd52 <krajcovicmatejk@gmail.com>
2026-09-08 18:35:45 +0200

    Update buttons.py

--- buttons.py
+++ buttons.py
@@ -19,23 +19,41 @@ def game_mode(fig, regime):
 
     ax2 = fig.add_axes([0.01, 0.9, 0.2, 0.05])
     pvc = Button(ax2, "PvC")
+    ax3 = fig.add_axes([0.01, 0.85, 0.2, 0.05])
+    pvp = Button(ax3, "PvP")
     ax2.set_visible(False)
+    ax3.set_visible(False)
 
+    # AI cast, vygeneroval templete a upravil som si ho podla potreby
+    """
+    Chcel som dropdown, ale nenapadal ma ako to spravit a nenašiel nič v manuali tkintera
+    Pre pochopenie toho čo to urobilo je spustenie dropdownu pomocou prepinania setvisible
+    Následne som si tam dodal resetovanie boardy a použil dalej ideu pri prepínaní farby
+    """
     def toggle(event):
-        ax2.set_visible(not ax2.get_visible())
-        button.label.set_text(
-            "Game mode: PvP ▲" if ax2.get_visible()
-            else "Game mode: PvP ▼"
-        )
+        swapper = not ax2.get_visible()
+        ax2.set_visible(swapper)
+        ax3.set_visible(swapper)
+        button.label.set_text("Game mode: chose ▲")
         fig.canvas.draw_idle()
 
-    def select_pvc(event):
-        regime("PvC")
+    def select_pvc(fig, event):
+        regime("PvC", fig)
         button.label.set_text("Game mode: PvC ▼")
         ax2.set_visible(False)
+        ax3.set_visible(False)
         fig.canvas.draw_idle()
 
-    button.on_clicked(toggle)
-    pvc.on_clicked(select_pvc)
+    def select_pvp(fig, event):
+        regime("PvP", fig)
+        button.label.set_text("Game mode: PvP ▼")
+        ax2.set_visible(False)
+        ax3.set_visible(False)
+        fig.canvas.draw_idle()
+    
 
-    return button, pvc
+    button.on_clicked(toggle)
+    pvc.on_clicked(lambda event: select_pvc(fig, event))
+    pvp.on_clicked(lambda event: select_pvp(fig, event))
+    # koniec AI casti
+    return button, pvc, pvp
